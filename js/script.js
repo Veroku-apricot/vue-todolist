c5fc9bf9d53768fd2238fd92c6a110e70b385ffa
Veronika <veronikakuchtova95@gmail.com>
2020-11-18 17:11:50 +0100

    Remove element done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-var app = new Vue ({
+var vm = new Vue ({
   el: '#app',
   data: {
-    text: 'hello',
+    text: '',
     toDos: []
 
   },
   methods: {
     addTo: function() {
       this.toDos.push(this.text)
+    },
+    remove: function(index) {
+      this.toDos.splice(index,1)
     }
   }
 })
