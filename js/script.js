4143c6f9ecf15d986b9d1e2b941648c94ef9e457
Veronika <veronikakuchtova95@gmail.com>
2020-11-19 15:22:17 +0100

    Finished chores message

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var vm = new Vue ({
   el: '#app',
   data: {
     text: '',
+    message: 'There is nothing to do...',
     toDos: []
 
   },
@@ -9,11 +10,13 @@ var vm = new Vue ({
     // Add element to list
     addTo: function() {
       this.text===''?'':this.toDos.push(this.text);
-      this.text = ''
+      this.text = '',
+      this.message = ''
     },
     // Remove element from list
     remove: function(index) {
-      this.toDos.splice(index,1)
+      this.toDos.splice(index,1);
+      this.toDos.length===0?this.message='Gratz, you finished your chores!':''
     }
   }
 })
