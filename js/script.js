9f01850a76dba37a0a6340e5da76d877ad415850
Veronika Kuchtova <71652367+Veroku-apricot@users.noreply.github.com>
2022-12-29 16:39:09 +0100

    Updated code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-var vm = new Vue ({
-  el: '#app',
-  data: {
-    text: '',
-    message: 'There is nothing to do...',
-    toDos: []
+// var vm = new Vue ({
+//   el: '#app',
+//   data: {
+//     text: '',
+//     message: 'There is nothing to do...',
+//     toDos: []
 
+//   },
+//   methods: {
+//     // Add element to list
+//     addTo: function() {
+//       this.text===''?'':this.toDos.push(this.text);
+//       this.text = '',
+//       this.message = ''
+//     },
+//     // Remove element from list
+//     remove: function(index) {
+//       this.toDos.splice(index,1);
+//       this.toDos.length===0?this.message='Congratulations, you finished your chores!':''
+//     }
+//   }
+// })
+
+
+const { createApp } = Vue
+createApp({
+  data() {
+    return {
+      text: '',
+      message: 'There is nothing to do...',
+      toDos: []
+    }
   },
   methods: {
     // Add element to list
@@ -16,7 +41,8 @@ var vm = new Vue ({
     // Remove element from list
     remove: function(index) {
       this.toDos.splice(index,1);
-      this.toDos.length===0?this.message='Gratz, you finished your chores!':''
+      this.toDos.length===0?this.message='Congratulations, you finished your chores!':''
     }
   }
-})
+
+}).mount('#app')
